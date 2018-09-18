93a41dbff4d352458859b366ad55129df87ce326
Teemu Pääkkönen <oamaok@gmail.com>
2018-09-18 09:17:51 +0300

    Add skeleton

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/', (ctx, next) => {
+
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
