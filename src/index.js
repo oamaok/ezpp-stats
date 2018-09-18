99893a872c49be885a7236e6f33d3f7aaafde06d
Teemu Pääkkönen <oamaok@gmail.com>
2018-09-18 09:54:41 +0300

    Add serverside react for templating

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-const Koa = require('koa');
-const Router = require('koa-router');
+import Koa from 'koa';
+import Router from 'koa-router';
+
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+
+function TestComponent() {
+  return <div>hello</div>;
+}
 
 const app = new Koa();
 const router = new Router();
 
 router.get('/', (ctx, next) => {
-
+  ctx.body = ReactDOMServer.renderToStaticMarkup(<TestComponent />)
 });
 
 app.use(router.routes());
 app.use(router.allowedMethods());
+app.listen(8080);
